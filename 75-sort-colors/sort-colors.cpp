55d0c042558b2cf000db3a62e94a26b899a2c223
Aman Kumar <163164299+amanyadav30922@users.noreply.github.com>
2026-07-13 23:27:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (78.98%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,20 +1,41 @@
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-    //     int cnt=0,cnt1=0,cnt2=0;
-    //     for(int i=0;i<nums.size();i++){
-    //         if(nums[i]==0) cnt++;
-    //         if(nums[i]==1) cnt1++;
-    //         if(nums[i]==2) cnt2++;
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int cnt=0,cnt1=0,cnt2=0;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==0) cnt++;
+//             if(nums[i]==1) cnt1++;
+//             if(nums[i]==2) cnt2++;
             
-    //     }
-    //      int i = 0;
+//         }
+//          int i = 0;
 
-    //     while (cnt--) nums[i++] = 0;
-    //     while (cnt1--) nums[i++] = 1;
-    //     while (cnt2--) nums[i++] = 2;
+//         while (cnt--) nums[i++] = 0;
+//         while (cnt1--) nums[i++] = 1;
+//         while (cnt2--) nums[i++] = 2;
+//     }
+   
+// };
+
+//optimal approach
+class Solution{
+    public:
+    void sortColors(vector<int>&nums){
+    // int low=0,mid=0,high=nums.size()-1;
+    // while(mid<=high){
+    //     if(nums[mid]==0){
+    //         swap(nums[mid],nums[low]);
+    //         low++;mid++;
+    //     }
+    //     else if(nums[mid]==1){
+    //         mid++;
+    //     }
+    //     else if(nums[mid]==2){
+    //         swap(nums[mid],nums[high]);
+    //         high--;
+    //     }
     // }
+
     sort(nums.begin(),nums.end());
     }
-    
 };
