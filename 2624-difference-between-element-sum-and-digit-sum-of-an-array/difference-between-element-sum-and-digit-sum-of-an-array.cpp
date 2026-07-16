afaf93e11cdc94b85e5409285d45c481a0909416
Aman Kumar <163164299+amanyadav30922@users.noreply.github.com>
2026-07-16 22:47:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (57.34%) - LeetSync

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.cpp
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.cpp
@@ -1,20 +1,37 @@
+// class Solution {
+// public:
+//     int differenceOfSum(vector<int>& nums) {
+//         int sum=0;int sum1=0;
+//         for(int i=0;i<nums.size();i++){
+//             sum+=nums[i];
+//         if(nums[i]>9 ){
+//             while(nums[i]>0){
+//             int a=nums[i]%10;
+//             sum1+=a;
+//             nums[i]/=10;
+//             }
+//         }
+//         else{
+//             sum1+=nums[i];
+//         }
+//         }
+//         return abs(sum-sum1);
+//     }
+// };
+
 class Solution {
 public:
     int differenceOfSum(vector<int>& nums) {
-        int sum=0;int sum1=0;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-        if(nums[i]>9 ){
-            while(nums[i]>0){
-            int a=nums[i]%10;
-            sum1+=a;
-            nums[i]/=10;
+        int sum = 0, digitSum = 0;
+
+        for (int x : nums) {
+            sum += x;
+            while (x > 0) {
+                digitSum += x % 10;
+                x /= 10;
             }
         }
-        else{
-            sum1+=nums[i];
-        }
-        }
-        return abs(sum-sum1);
+
+        return abs(sum - digitSum);
     }
 };
