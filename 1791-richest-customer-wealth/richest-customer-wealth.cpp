9a1b5a485fc4283d670be806c95bd8a218d0ab1f
Aman Kumar <163164299+amanyadav30922@users.noreply.github.com>
2026-07-17 13:43:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (20.47%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.cpp
+++ 1791-richest-customer-wealth/richest-customer-wealth.cpp
@@ -1,17 +1,34 @@
+// class Solution {
+// public:
+//     int maximumWealth(vector<vector<int>>& accounts) {
+//         vector<int>ans;
+//         for(int i=0;i<accounts.size();i++){
+            
+           
+//             int sum=0;
+//             for(int j=0;j<accounts[i].size();j++){
+//                 sum+=accounts[i][j];
+//             }
+//             ans.push_back(sum);
+//         }
+//         sort(ans.begin(),ans.end());
+//         return ans[ans.size()-1];
+//     }
+// };
+
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        vector<int>ans;
-        for(int i=0;i<accounts.size();i++){
-            
-           
-            int sum=0;
-            for(int j=0;j<accounts[i].size();j++){
-                sum+=accounts[i][j];
+        int mx = 0;
+
+        for (int i = 0; i < accounts.size(); i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].size(); j++) {
+                sum += accounts[i][j];
             }
-            ans.push_back(sum);
+            mx = max(mx, sum);
         }
-        sort(ans.begin(),ans.end());
-        return ans[ans.size()-1];
+
+        return mx;
     }
 };
