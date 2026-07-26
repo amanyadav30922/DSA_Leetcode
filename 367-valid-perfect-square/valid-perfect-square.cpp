609a660583a758eb397f590bcb9cf129f6a1255f
Aman Kumar <163164299+amanyadav30922@users.noreply.github.com>
2026-07-26 21:24:03 +0530

    Time: 2443 ms (5.00%) | Memory: 7.9 MB (18.81%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        int root=sqrt(num);
-        return 1LL *root*root==num;
+        // int root=sqrt(num);
+        // return 1LL *root*root==num;
+        for(long long i=1;i<=num;i++){
+            if(num==i*i){
+                return true;
+            }
+        }
+        return false;
     }
 };
